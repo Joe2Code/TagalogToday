590178e7b33673626af38809ed02a9e04471b137
Joseph Mennemeier <jmennemeier@aol.com>
2016-08-28 22:47:13 -0500

    Initial commit

--- TagalogToday/ViewController.swift
+++ TagalogToday/ViewController.swift
@@ -8,11 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var words = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard()
+        
+        if let savedWords = defaults.object(forKey: "Words") as? [String] {
+            words = savedWords
+        } else {
+            saveInitialValues(to: defaults)
+        }
+        
+        let titleAttributes = [NSFontAttributeName: UIFont(name: "AmericanTypewriter", size: 22)!]
+        
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        title = "TAGALOG TODAY"
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func saveInitialValues(to defaults: UserDefaults) {
+        words.append("one::isa")
+        words.append("two::dalawa")
+        words.append("three::tatlo")
+        words.append("four::apat")
+        words.append("good morning::magandang umaga")
+        words.append("hello::mabuhay")
+        words.append("How are you?::kamusta ka")
+        words.append("good evening::magandang gabi")
+        
+        defaults.set(words, forKey: "Words")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        let word = words[indexPath.row]
+        let split = word.components(separatedBy: "::")
+        
+        cell.textLabel?.text = split[0]
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.detailTextLabel?.text == "" {
+                let word = words[indexPath.row]
+                let split = word.components(separatedBy: "::")
+                cell.detailTextLabel?.text = split[1]
+            } else {
+                cell.detailTextLabel?.text = ""
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +87,17 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
